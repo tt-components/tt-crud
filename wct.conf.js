13d24d441b1b34f1cb3e649da3a601f5c41ddbe5
jmorille <jmorille@gmail.com>
2015-02-06 23:37:35 +0100

    test

---
+++ wct.conf.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Whether the browser should remain open after running tests:
+  'persistent': false,
+  // Display test results in expanded form:
+  'expanded': false,
+
+  // Output verbosity:
+  'verbose': false 
+
+};
